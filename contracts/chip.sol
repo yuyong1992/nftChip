325b8762ff9babce5f482fa8035e60f548f3016c
Yuyong <1305703064@qq.com>
2021-07-14 18:23:26 +0800

    优化函数

--- contracts/chip.sol
+++ contracts/chip.sol
@@ -5,17 +5,16 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 
 contract Chip is Ownable, ERC721{
-    // 合约的所有者，存在变量中方便有需要直接调用
     address ownerOfContract = msg.sender;
 
-    // token和创作者的对应
     mapping(uint256 => address) authorOf;
 
     // 定义事件
-    // event Mint(address indexed to, address indexed author, uint256 indexed token_id);
-    // event create(address indexed to, address indexed author, uint256 indexed token_id);
-    // event ChangeAuthor(address indexed Modifier, address befor_addr, address indexed after_addr, uint256 indexed token_id);
-    // event Buy(address indexed from, address indexed to, uint256 token_id);
+    event Mint(address indexed to, address indexed author, uint256 indexed token_id);
+    event Create(address indexed to, address indexed author, uint256 indexed token_id);
+    event ChangeAuthor(address indexed Modifier, address befor_addr, address indexed after_addr, uint256 indexed token_id);
+    event Buy(address indexed from, address indexed to, uint256 token_id);
+    event Withdraw(address apply_addr, address to_addr, uint value);
     
 
     constructor() ERC721("Nft.Chip", "CHIP") {
@@ -23,7 +22,7 @@ contract Chip is Ownable, ERC721{
         // _setBaseURI("https://date.kie.codes/token/");
 
         // 这里是初始化的时候 mint 的token，可以初始化多个
-        mint(msg.sender, 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2, 0);
+        mint(msg.sender,0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2, 0);
         mint(msg.sender,0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2, 1); 
         mint(msg.sender,0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2, 2); 
         mint(msg.sender,0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2, 3); 
@@ -38,15 +37,12 @@ contract Chip is Ownable, ERC721{
         
     }
     
-    function withdraw () onlyOwner public{
-        msg.sender.transfer(address(this).balance);
+    function withdraw () onlyOwner public payable {
+        uint value = address(this).balance;
+        address(int160(ownerOfContract)).transfer(value);
+        emit Withdraw(msg.sender, ownerOfContract, value);
     }
     
-    /*
-     *  设置获取token元数据的接口
-     *  例如：baseURL为：http:www.baseuri.com
-     *  id为 1 的token会自动从：http:www.baseuri.com/1 获取元数据
-    */
     function setBaseURI(string memory baseURI) public onlyOwner {
         // require(msg.sender == ownerOfContract, "only the owner of contract can set the baseURI");
         _setBaseURI(baseURI);
@@ -59,18 +55,31 @@ contract Chip is Ownable, ERC721{
     function mint(address to, address author, uint token_id) public onlyOwner {
         authorOf[token_id] = author;
         _safeMint(to, token_id);
-        // emit Mint(to, author, token_id);
+        emit Mint(to, author, token_id);
     }
 
     function create(address author, uint token_id) external payable {
-        // 需要确定创建NFT需不需要花钱
         mint(msg.sender, author, token_id);
-        address(uint160(owner())).transfer(msg.value);
-        // emit create(msg.sender, author, token_id);
+        emit Create(msg.sender, author, token_id);
     }
-
-    function ownerOfChip(uint token_id) public view returns(address) {
-        return ownerOf(token_id);
+    
+    function buy (uint token_id) public payable {
+        require(_exists(token_id), "token not minted");
+        require(ownerOf(token_id) != msg.sender, "already the owner");
+        address(uint160(ownerOf(token_id))).transfer(msg.value/100 * 88);
+        address(uint160(authorOf[token_id])).transfer(msg.value/100 * 6);
+        address from_addr = ownerOf(token_id);
+        transferToken(from_addr, msg.sender, token_id);
+        emit Buy(from_addr, msg.sender, token_id);
+    }
+    
+    function changeAuthor(uint token_id, address new_author) public {
+        require(_exists(token_id), "token id not exists");
+        require(new_author != address(0), "address of new author is zero");
+        require(msg.sender == ownerOfContract, "have no authority to change the author");
+        address author_old = authorOf[token_id];
+        authorOf[token_id] = new_author;
+        emit ChangeAuthor(msg.sender, author_old, new_author, token_id);
     }
     
     function getauthor (uint _token_id) public view returns(address) {
@@ -78,31 +87,8 @@ contract Chip is Ownable, ERC721{
         return authorOf[_token_id];
     }
     
-    function getOwnerOfContract () public view returns (address){
-        return ownerOfContract;
-    }
-    
-    function changeAuthor(uint token_id, address new_author) public {
-        require(_exists(token_id), "token id not exists");
-        require(new_author != address(0), "address of new author is zero");
-        require(msg.sender == owner(), "have no authority to change the author");
-        // address author_old = authorOf[token_id];
-        authorOf[token_id] = new_author;
-        // emit ChangeAuthor(msg.sender, author_old, new_author, token_id);
-    }
-    
-    function transfer(address from_addr, address to_addr, uint token_id) internal {
+    function transferToken(address from_addr, address to_addr, uint token_id) internal {
         _transfer(from_addr, to_addr, token_id);
-    }
-
-    function buy (uint token_id) public payable {
-        require(_exists(token_id), "token not minted");
-        require(ownerOfChip(token_id) != msg.sender, "already the owner");
-        // 原owner:创作者:平台 = 88:6:6
-        address(uint160(ownerOfChip(token_id))).transfer(msg.value/100 * 88);
-        address(uint160(authorOf[token_id])).transfer(msg.value/100 * 6);
-        transfer(ownerOfChip(token_id), msg.sender, token_id);
-        // emit Buy(from_addr, msg.sender, token_id);
     }
     
     function balanceOfContract() public onlyOwner view returns(uint)  {
